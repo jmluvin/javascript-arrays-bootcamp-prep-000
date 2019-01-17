3da383802fe78d9cd2b21c9e7c071ab5db71932c
Matthew Luvin <jmluvin@gmail.com>
2019-01-17 17:24:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(chocolateBars, herseys) { return [herseys,...chocolateBars] }
+function destructivelyAddElementToBeginningOfArray(chocolateBars, herseys) { chocolateBars.unshift(herseys)
+return chocolateBars}
+function addElementToEndOfArray(chocolateBars, herseys) { return [...chocolateBars, herseys] }
+function destructivelyAddElementToEndOfArray(chocolateBars, herseys) { chocolateBars.push(herseys)
+return chocolateBars}
