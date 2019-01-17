c92bfd90c2843f7074e6780fe5f48d80b23b35d0
Matthew Luvin <jmluvin@gmail.com>
2019-01-17 17:39:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,6 @@ return chocolateBars}
 function addElementToEndOfArray(chocolateBars, herseys) { return [...chocolateBars, herseys] }
 function destructivelyAddElementToEndOfArray(chocolateBars, herseys) { chocolateBars.push(herseys)
 return chocolateBars}
+function accessElementInArray(chocolateBars, index) {
+  return console.log(chocolateBars[0])
+}
