19fe757695d3e09b8c12bc1e4a71a3a12edcd5ce
Matthew Luvin <jmluvin@gmail.com>
2019-01-17 18:51:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) { return [element,...array] }
-function destructivelyAddElementToBeginningOfArray(chocolateBars, herseys) { chocolateBars.unshift(herseys)
+function destructivelyAddElementToBeginningOfArray(array, element) { array.unshift(element)
 return chocolateBars}
 function addElementToEndOfArray(chocolateBars, herseys) { return [...chocolateBars, herseys] }
 function destructivelyAddElementToEndOfArray(chocolateBars, herseys) { chocolateBars.push(herseys)
