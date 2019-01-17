cf2080e5b9e4834554017e6ffb3cbcee67eb1b5f
Matthew Luvin <jmluvin@gmail.com>
2019-01-17 18:10:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,8 @@ function addElementToEndOfArray(chocolateBars, herseys) { return [...chocolateBa
 function destructivelyAddElementToEndOfArray(chocolateBars, herseys) { chocolateBars.push(herseys)
 return chocolateBars}
 function accessElementInArray(chocolateBars, snickers) {
-  console.log(chocolateBars[0]);
+  console.log(chocolateBars[0])
+  return 
 }
 function removeElementFromBeginningOfArray(chocolateBars) { return chocolateBars.slice(1) }
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) { chocolateBars.shift()
